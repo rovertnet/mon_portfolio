1de3e14e04c0319bb23192089ce0657e83045de8
Robert Matundu <matundukabamba@gmail.com>
2023-07-08 23:08:24 +0200

    un autre comit

--- src/composants/about/About.jsx
+++ src/composants/about/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./about.css";
+import aboutImg from "../../assets/moi.png";
+import CV from "../../assets/CV_Robert.pdf";
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about section" id="about">
+        <h2 className="section_title">A propos de moi</h2>
+        <span className="section_subtitle">mon introduction</span>
+        <div className="about_container container grid">
+            <img src={aboutImg} alt="" className="about_img" />
+            <div className="about_data">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
