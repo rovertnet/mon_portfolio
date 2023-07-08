239041233135dfe43e3d56e30ee3b89e7a0b816b
Robert Matundu <matundukabamba@gmail.com>
2023-07-08 18:56:56 +0200

    mon troisieme comit

--- src/composants/body/Social.jsx
+++ src/composants/body/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home_social">
-          <a href="https://www.instagram.com/" className="home_social-icon" target='_blank'>
+        <a href="https://www.instagram.com/" className="home_social-icon" target='blank'>
             <i className="uil uil-instagram"></i>
         </a>
-          <a href="https://linkedin.com/" className="home_social-icon" target='_blank'>
+          <a href="https://linkedin.com/" className="home_social-icon" target='blank'>
             <i className="uil uil-linkedin"></i>
         </a>
-          <a href="https://github.com/" className="home_social-icon" target='_blank'>
+          <a href="https://github.com/" className="home_social-icon" target='blank'>
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
