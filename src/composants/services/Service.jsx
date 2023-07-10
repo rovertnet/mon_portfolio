b42a30866ba63f32db4cd299712a46c7869d668e
Robert Matundu <matundukabamba@gmail.com>
2023-07-10 17:35:13 +0200

    un autre comite

--- src/composants/services/Service.jsx
+++ src/composants/services/Service.jsx
@@ -1,8 +1,187 @@
-import React from 'react'
+import React from 'react';
+import "./service.css";
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <div className="services section" id="services">
+          <h2 className="section_title">Services</h2>
+          <span className="section_subtitle">introduction</span>
+
+        <div className="services_container container grid">
+            <div className="services_content">
+                <div>
+                    <i className="uil uil-arrow services-icon"></i>
+                    <h3 className="services_title">UI/UX <br /> Designer </h3>
+                </div>
+                <span className="services_button">
+                    plus{""} <i className="uil uil-arrow-right"></i>
+                </span>
+                <div className="services_modal">
+                    <div className="services_modal-content">
+                        <i className="uil uil-times"></i>
+                        <h3 className="services_modal-title"></h3>
+                        <p className="services_modal-description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Nisi ex sapiente cum harum corporis, molestiae repudiandae saepe, 
+                            voluptatem libero, unde hic odio incidunt natus velit. 
+                            Ea, at. Cumque, numquam suscipit?
+                        </p>
+                        <ul className="services_modal-services grid">
+                            <li className="services_modal-service">
+                                <i className="uil uil-check-circle services_modal-icon"></i>
+                                <p className="services_modal-info">Je développe les interfaces utilisateurs (UI)</p>
+                            </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Développement des pages web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites web & application web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites WordPress</p>
+                              </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services_content">
+                  <div>
+                      <i className="uil uil-arrow services-icon"></i>
+                      <h3 className="services_title">UI/UX <br /> Designer </h3>
+                  </div>
+                  <span className="services_button">
+                      plus{""} <i className="uil uil-arrow-right"></i>
+                  </span>
+                  <div className="services_modal">
+                      <div className="services_modal-content">
+                          <i className="uil uil-times"></i>
+                          <h3 className="services_modal-title"></h3>
+                          <p className="services_modal-description">
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                              Nisi ex sapiente cum harum corporis, molestiae repudiandae saepe,
+                              voluptatem libero, unde hic odio incidunt natus velit.
+                              Ea, at. Cumque, numquam suscipit?
+                          </p>
+                          <ul className="services_modal-services grid">
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Je développe les interfaces utilisateurs (UI)</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Développement des pages web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites web & application web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites WordPress</p>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+            </div>
+
+            <div className="services_content">
+                  <div>
+                      <i className="uil uil-arrow services-icon"></i>
+                      <h3 className="services_title">UI/UX <br /> Designer </h3>
+                  </div>
+                  <span className="services_button">
+                      plus{""} <i className="uil uil-arrow-right"></i>
+                  </span>
+                  <div className="services_modal">
+                      <div className="services_modal-content">
+                          <i className="uil uil-times"></i>
+                          <h3 className="services_modal-title"></h3>
+                          <p className="services_modal-description">
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                              Nisi ex sapiente cum harum corporis, molestiae repudiandae saepe,
+                              voluptatem libero, unde hic odio incidunt natus velit.
+                              Ea, at. Cumque, numquam suscipit?
+                          </p>
+                          <ul className="services_modal-services grid">
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Je développe les interfaces utilisateurs (UI)</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Développement des pages web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites web & application web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites WordPress</p>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+            </div>
+
+            <div className="services_content">
+                  <div>
+                      <i className="uil uil-arrow services-icon"></i>
+                      <h3 className="services_title">Référencement <br /> SEO </h3>
+                  </div>
+                  <span className="services_button">
+                      plus{""} <i className="uil uil-arrow-right"></i>
+                  </span>
+                  <div className="services_modal">
+                      <div className="services_modal-content">
+                          <i className="uil uil-times"></i>
+                          <h3 className="services_modal-title"></h3>
+                          <p className="services_modal-description">
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                              Nisi ex sapiente cum harum corporis, molestiae repudiandae saepe,
+                              voluptatem libero, unde hic odio incidunt natus velit.
+                              Ea, at. Cumque, numquam suscipit?
+                          </p>
+                          <ul className="services_modal-services grid">
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Je développe les interfaces utilisateurs (UI)</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Développement des pages web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites web & application web</p>
+                              </li>
+
+                              <li className="services_modal-service">
+                                  <i className="uil uil-check-circle services_modal-icon"></i>
+                                  <p className="services_modal-info">Création des sites WordPress</p>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
