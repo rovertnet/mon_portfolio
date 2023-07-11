0759b71a5a01589be4841e035564937eb2caa04d
Robert Matundu <matundukabamba@gmail.com>
2023-07-12 00:02:54 +0200

    un autre comite

--- src/composants/qualification/Qualification.jsx
+++ src/composants/qualification/Qualification.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./qualification.css";
 
 const Qualification = () => {
+    const [Toggle, setToggle] = useState(0);
+    const ToggleTab = (index) => {
+        setToggle(index)
+    }
   return (
     <section className="qualification section">
         <h2 className="section_title">Qualification</h2>
@@ -9,19 +13,25 @@ const Qualification = () => {
 
         <div className="qualification_container container">
             <div className="qualification_tabs">
-                <div className="qualification_button button--flex">
+                <div className={Toggle === 1 ? "qualification_button button--flex" :
+                   "qualification_button button--flex"}>
                     <i className="uil uil-graduation-cap qualificatio_icon"></i>{""}
                     Education
                 </div>
 
-                <div className="qualification_button qualification_active button--flex">
+                  <div className={Toggle === 2 ? "qualification_button button--flex" :
+                      "qualification_button button--flex"}>
                       <i className="uil uil-briefcase qualificatio_icon"></i>{""}
                       Expériences
                 </div>
             </div>
 
             <div className="qualification_sections">
-                <div className="qualification_content qualification_content-active">
+                  <div className={
+                    Toggle === 1 ? 
+                     "qualification_content qualification_content-active" : 
+                     "qualification_content qualification_content-active"}
+                      onClick={() => ToggleTab(1)}>
                     <div className="qualification_data">
                           <div></div>
 
@@ -101,7 +111,10 @@ const Qualification = () => {
                     </div>
                 </div>
 
-                <div className="qualification_content">
+                  <div className={Toggle === 2 ? 
+                      "qualification_content" :       
+                      "qualification_content"}
+                      onClick={() => ToggleTab(2)}>
                       <div className="qualification_data">
                           <div></div>
 
