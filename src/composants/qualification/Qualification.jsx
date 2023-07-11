b7605c1465c58f5c9bd7371004008d02719722b0
Robert Matundu <matundukabamba@gmail.com>
2023-07-11 18:45:14 +0200

    un autre comite

--- src/composants/qualification/Qualification.jsx
+++ src/composants/qualification/Qualification.jsx
@@ -21,23 +21,144 @@ const Qualification = () => {
             </div>
 
             <div className="qualification_sections">
-                <div className="qualification_content">
+                <div className="qualification_content qualification_content-active">
                     <div className="qualification_data">
                         <div>
                             <span className="qualification_rounder"></span>
                             <span className="qualification_line"></span>
                         </div>
                         <div>
-                            <h3 className="qualification_title"></h3>
+                            <h3 className="qualification_title">Génie Logiciel</h3>
                             <span className="qualification_subtitle">
                                 Institut Superieur d'Informatique Programmation & Analyse
                             </span>
                             <div className="qualification_calender">
-                                <i className="uil uil-calender-alt"></i>
-                                2021 - Présente
+                                <i className="uil uil-calendar-alt"></i>
+                                2022 - Licence
                             </div>
                         </div>
                     </div>
+                    
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Développement web</h3>
+                              <span className="qualification_subtitle">
+                                  Open classRoom - à distance
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2021 - Certificat
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Informatique de gestion</h3>
+                              <span className="qualification_subtitle">
+                                  Institut Superieur d'Informatique Programmation & Analyse
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2020 - Graduat
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Développement de logiciels</h3>
+                              <span className="qualification_subtitle">
+                                  Centre de formation - CECONTEC/Kinshasa
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2018 - Certificat
+                              </div>
+                          </div>
+                      </div>
+                </div>
+
+                <div className="qualification_content">
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Développeur Web | Web Master</h3>
+                              <span className="qualification_subtitle">
+                                  Mans Consulting
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2023
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Développement web</h3>
+                              <span className="qualification_subtitle">
+                                  MAFRICOM
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2022
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Développeur Web</h3>
+                              <span className="qualification_subtitle">
+                                  La Colomb Transfert
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2022 
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="qualification_data">
+                          <div>
+                              <span className="qualification_rounder"></span>
+                              <span className="qualification_line"></span>
+                          </div>
+                          <div>
+                              <h3 className="qualification_title">Développeur Web</h3>
+                              <span className="qualification_subtitle">
+                                  Bestech Consult
+                              </span>
+                              <div className="qualification_calender">
+                                  <i className="uil uil-calendar-alt"></i>
+                                  2020 - 2023
+                              </div>
+                          </div>
+                      </div>
                 </div>
             </div>
         </div>
