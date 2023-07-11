6e1984704516feb74c93f518a0f891bf3c591e24
Robert Matundu <matundukabamba@gmail.com>
2023-07-11 16:18:42 +0200

    un autre comite

--- src/composants/qualification/Qualification.jsx
+++ src/composants/qualification/Qualification.jsx
@@ -4,8 +4,16 @@ import "./qualification.css";
 const Qualification = () => {
   return (
     <section className="qualification section">
-        <h2 className="section_title">A propos de moi</h2>
+        <h2 className="section_title">Qualification</h2>
         <span className="section_subtitle">mon introduction</span>
+
+        <div className="qualification_container container">
+            <div className="qualification_tabs">
+                <div className="qualification_button button--flex">
+                    <i className="uil uil- qualificatio_icon"></i>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
