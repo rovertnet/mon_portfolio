cb02f989fe84d18197e3e8283f8d40e7eead8673
Robert Matundu <matundukabamba@gmail.com>
2023-07-13 01:39:44 +0200

    un autre comite

--- src/composants/temoignage/Temoignage.jsx
+++ src/composants/temoignage/Temoignage.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import "./temoignage.css";
+import { Data } from './Data';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import {Pagination} from "swiper";
 
 const Temoignage = () => {
   return (
     <section className="testimonial container section">
-          <h2 className="section_title">Services</h2>
+          <h2 className="section_title">Clients</h2>
           <span className="section_subtitle">introduction</span>
 
-          <div className="testimonial_container"></div>
+          <Swiper className="testimonial_container"
+                  loop={true}
+                  grabCursor={true}
+                  spaceBetween={24}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  breakpoints={{
+                    576:{
+                        slidesPerView: 2,
+                    },
+                    768:{
+                        slidesPerView: 2,
+                        spaceBetween: 48,   
+                    }
+                  }}
+          >
+            {Data.map(({id, image, title, description})=>{
+                return(
+                    <SwiperSlide className="testimonial card" key={id}>
+                        <img src={image} alt="" className="testimonial_img" />
+
+                        <h3 className="testimonial_name">{title}</h3>
+                        <p className="testimonial_description">{description}</p>
+                    </SwiperSlide>
+                )
+            })}
+          </Swiper>
     </section>
   )
 }
