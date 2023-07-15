3f9078658c11511bd6a9e26e202766faddd598e9
Robert Matundu <matundukabamba@gmail.com>
2023-07-15 02:41:46 +0200

    un autre comite

--- src/composants/contact/Contact.jsx
+++ src/composants/contact/Contact.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_47jbogf', 
+      'template_hndm7dd', 
+      form.current, 
+      'ghCt_42zK73_zQWBW'
+      )
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
   return (
     <section className="contact section" id="contact">
           <h2 className="section_title">Contactez moi</h2>
@@ -32,15 +50,16 @@ const Contact = () => {
                       <i className="bx bxl-messenger contact_card-icon"></i>
                       <h3 className="contact_card-title">Messenger</h3>
                       <span className="contact_card-data">IL Robert Kabamba</span>
-                      <a href="" className="contact_button">Ecrivez-moi <i className="bx bx-right-arrow-alt 
-                                contact_button-icon"></i> </a>
+                      <a href="#contact" className="contact_button">Ecrivez-moi 
+                         <i className="bx bx-right-arrow-alt contact_button-icon"></i> 
+                      </a>
                   </div>
                 </div>
             </div>
 
            <div className="contact_content">
               <h3 className="contact_title">Faites moi part de votre projet</h3>
-              <form className="contact_form">
+              <form ref={form} onSubmit={sendEmail} className="contact_form">
                 <div className="contact_form-div">
                   <label className="contact_form-tag">Nom</label>
                   <input
