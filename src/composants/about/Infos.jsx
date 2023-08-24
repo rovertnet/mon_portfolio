48c94e00baa99b90bb52fca10462a93421b7ce88
Robert Matundu <matundukabamba@gmail.com>
2023-08-24 19:27:39 +0200

    ajout d'un élemen

--- src/composants/about/Infos.jsx
+++ src/composants/about/Infos.jsx
@@ -3,25 +3,31 @@ import React from 'react'
 const Infos = () => {
   return (
     <div className="about_info grid">
-        <div className="about_box">
-            <i className="bx bx-award"></i>
-            <h3 className="about_title">Expériences</h3>
-            <span className="about_subtitle">5 ans de travail</span>
-        </div>
+      <div className="about_box">
+        <i className="bx bx-award"></i>
+        <h3 className="about_title">Expériences</h3>
+        <span className="about_subtitle">5 ans de travail</span>
+      </div>
 
-          <div className="about_box">
-              <i className="bx bx-support"></i>
-              <h3 className="about_title">Supports</h3>
-              <span className="about_subtitle">3/5</span>
-          </div>
+      <div className="about_box">
+        <i className="bx bx-support"></i>
+        <h3 className="about_title">Supports</h3>
+        <span className="about_subtitle">3/5</span>
+      </div>
 
-          <div className="about_box">
-              <i className="bx bx-briefcase-alt"></i> 
-              <h3 className="about_title">Projets terminés</h3>
-              <span className="about_subtitle">5 projets</span>
-          </div>
+      <div className="about_box">
+        <i className="bx bx-briefcase-alt"></i>
+        <h3 className="about_title">Projets terminés</h3>
+        <span className="about_subtitle">5 projets</span>
+      </div>
+
+      <div className="about_box">
+        <i className="bx bx-signal-5"></i>
+        <h3 className="about_title">Stat</h3>
+        <span className="about_subtitle">5/5</span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Infos
